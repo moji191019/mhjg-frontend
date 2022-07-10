198a70cd9371751fd98985d8bd5b257f14332923
mocci <tjdwhd3345@gmail.com>
2022-07-10 18:37:38 +0900

    install package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    { id: 0, name: 'mo' },
+    { id: 1, name: 'hong' },
+    { id: 2, name: 'kim' },
+  ]);
+  const test = () => {
+    console.log('asdf');
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          hi react!! <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
