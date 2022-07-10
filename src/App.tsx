e9986ee351623f352df2432dab7b0b6072de2183
mocci <tjdwhd3345@gmail.com>
2022-07-10 22:39:20 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [items, setItems] = useState([
-    { id: 0, name: 'mo' },
-    { id: 1, name: 'hong' },
-    { id: 2, name: 'kim' },
-  ]);
-  const test = () => {
-    console.log('asdf');
-  };
   return (
     <div className="App">
       <header className="App-header">
