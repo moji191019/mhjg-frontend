f4eb9e9b786b0fba0b98ec6f6293e6be8b982f2b
mocci <tjdwhd3345@gmail.com>
2022-07-17 02:05:18 +0900

    react-router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { Box, ButtonGroup } from '@mui/material';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Box, ButtonGroup, Container } from '@mui/material';
 import styles from './App.module.scss';
 import BaseButton from './components/baseComp/BaseButton';
 import BaseInput from './components/baseComp/BaseInput';
 import Header from './header/Header';
 import SearchBox from './search/SearchBox';
 
-const App = () => {
+const ComponentSample = () => {
   const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     console.log('handleButtonClick', event.target);
   };
@@ -14,38 +15,49 @@ const App = () => {
     console.log('handleInputChange', event.target.value);
   };
   return (
-    <div className="App">
+    <>
+      <section className={styles.buttonSection}>
+        <BaseButton text="contained 버튼" variant="contained" onClickHandler={handleButtonClick} />
+        <BaseButton text="outelined 버튼" variant="outlined" onClickHandler={handleButtonClick} />
+        <BaseButton text="text 버튼" variant="text" onClickHandler={handleButtonClick} />
+        <ButtonGroup>
+          <BaseButton text="ButtonGroup 버튼" variant="contained" onClickHandler={handleButtonClick} />
+          <BaseButton text="ButtonGroup 버튼" variant="outlined" onClickHandler={handleButtonClick} />
+          <BaseButton text="ButtonGroup 버튼" variant="text" onClickHandler={handleButtonClick} />
+        </ButtonGroup>
+      </section>
+      <section className={styles.inputSection}>
+        <Box>
+          <BaseInput label="filled 인풋" defaultValue=" 기본값" variant="filled"></BaseInput>
+          <BaseInput label="outlined 인풋" defaultValue="" variant="outlined"></BaseInput>
+          <BaseInput label="standard 인풋" defaultValue="" variant="standard"></BaseInput>
+          <BaseInput label="helperText 인풋" helperText="도움말같은거 적는 곳" variant="outlined"></BaseInput>
+        </Box>
+        <Box>
+          <BaseInput label="small 인풋" size="small" variant="outlined"></BaseInput>
+          <BaseInput label="normal 인풋" size="medium" variant="outlined"></BaseInput>
+        </Box>
+        <Box>
+          <BaseInput label="controlled 인풋" onChangeHandler={handleInputChange} variant="outlined"></BaseInput>
+          <BaseInput label="uncontrolled 인풋" variant="outlined"></BaseInput>
+        </Box>
+      </section>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
       <Header />
-      <main>
-        <SearchBox></SearchBox>
-        <section className={styles.buttonSection}>
-          <BaseButton text="contained 버튼" variant="contained" onClickHandler={handleButtonClick} />
-          <BaseButton text="outelined 버튼" variant="outlined" onClickHandler={handleButtonClick} />
-          <BaseButton text="text 버튼" variant="text" onClickHandler={handleButtonClick} />
-          <ButtonGroup>
-            <BaseButton text="ButtonGroup 버튼" variant="contained" onClickHandler={handleButtonClick} />
-            <BaseButton text="ButtonGroup 버튼" variant="outlined" onClickHandler={handleButtonClick} />
-            <BaseButton text="ButtonGroup 버튼" variant="text" onClickHandler={handleButtonClick} />
-          </ButtonGroup>
-        </section>
-        <section className={styles.inputSection}>
-          <Box>
-            <BaseInput label="filled 인풋" defaultValue=" 기본값" variant="filled"></BaseInput>
-            <BaseInput label="outlined 인풋" defaultValue="" variant="outlined"></BaseInput>
-            <BaseInput label="standard 인풋" defaultValue="" variant="standard"></BaseInput>
-            <BaseInput label="helperText 인풋" helperText="도움말같은거 적는 곳" variant="outlined"></BaseInput>
-          </Box>
-          <Box>
-            <BaseInput label="small 인풋" size="small" variant="outlined"></BaseInput>
-            <BaseInput label="normal 인풋" size="medium" variant="outlined"></BaseInput>
-          </Box>
-          <Box>
-            <BaseInput label="controlled 인풋" onChangeHandler={handleInputChange} variant="outlined"></BaseInput>
-            <BaseInput label="uncontrolled 인풋" variant="outlined"></BaseInput>
-          </Box>
-        </section>
-      </main>
-    </div>
+      <Container component="main">
+        <Routes>
+          <Route path="/" element={<SearchBox />} />
+          <Route path="home" element={<SearchBox />} />
+          <Route path="sample" element={<ComponentSample />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink as RouterNavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import HomeIcon from '@mui/icons-material/Home';
 import Box from '@mui/material/Box';
@@ -18,13 +19,17 @@ const Header = () => {
         borderBottom: '1px solid gray',
       }}
     >
-      <Link variant="h4" sx={{ display: 'flex', alignItems: 'center' }}>
+      <Link component={RouterNavLink} to="/" variant="h4" sx={{ display: 'flex', alignItems: 'center' }}>
         <HomeIcon sx={{ fontSize: 'inherit' }} />
         LOGO
       </Link>
       <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-        <Link variant="h6">메뉴1</Link>
-        <Link variant="h6">메뉴2</Link>
+        <Link variant="h6" component={RouterNavLink} to="/home">
+          home
+        </Link>
+        <Link variant="h6" component={RouterNavLink} to="/sample">
+          component sample
+        </Link>
         <Link variant="h6">메뉴3</Link>
         <Link variant="h6">메뉴4</Link>
       </Box>
